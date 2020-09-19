fa059249031f22a7334b192af8ed9a2aada2eff1
IDragnev <iddragnev@gmail.com>
2020-09-19 17:26:13 +0300

    Clean app/main.cpp

--- app/main.cpp
+++ app/main.cpp
@@ -1,19 +1,3 @@
-#include <iostream>
-
-#include "core/Ray.hpp"
-#include "core/Bounds2.hpp"
-#include "core/Matrix4x4.hpp"
-#include "core/Quaternion.hpp"
-
-namespace pbrt = idragnev::pbrt;
-
 int main() {
-    const auto matrix = pbrt::Matrix4x4{};
-    const auto inversed = pbrt::inverse(matrix);
-    const auto transposed = pbrt::transpose(matrix);
-
-    assert(matrix == inversed);
-    assert(matrix == transposed);
-
     return 0;
 }
