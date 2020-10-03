f873e3dbb9e015bc07b58a3a53776a1cffd7ec23
IDragnev <iddragnev@gmail.com>
2020-10-03 17:02:26 +0300

    Update Interval's unit tests
    Use Interval of integers for all tests.

--- tests/core/interval.cpp
+++ tests/core/interval.cpp
@@ -3,89 +3,91 @@
 
 namespace pbrt = idragnev::pbrt;
 
+using Interval = pbrt::Interval<int>;
+
 TEST_CASE("construction from two values does not depend on"
           "arguments order") {
-    const auto i1 = pbrt::Intervalf{1.f, 0.f};
-    const auto i2 = pbrt::Intervalf{0.f, 1.f};
+    const auto i1 = Interval{1, 0};
+    const auto i2 = Interval{0, 1};
 
-    CHECK(i1.low() == 0.f);
-    CHECK(i1.high() == 1.f);
+    CHECK(i1.low() == 0);
+    CHECK(i1.high() == 1);
 
-    CHECK(i2.low() == 0.f);
-    CHECK(i2.high() == 1.f);
+    CHECK(i2.low() == 0);
+    CHECK(i2.high() == 1);
 }
 
 TEST_CASE("multiplication") {
-    const auto a = pbrt::Intervalf{0.f, 5.f};
-    const auto b = pbrt::Intervalf{2.f, 10.f};
+    const auto a = Interval{0, 5};
+    const auto b = Interval{2, 10};
 
     const auto c = a * b;
 
-    CHECK(c.low() == 0.f);
-    CHECK(c.high() == 50.f);
+    CHECK(c.low() == 0);
+    CHECK(c.high() == 50);
 }
 
 TEST_CASE("sum") {
-    const auto a = pbrt::Intervalf{0.f, 5.f};
-    const auto b = pbrt::Intervalf{2.f, 10.f};
+    const auto a = Interval{0, 5};
+    const auto b = Interval{2, 10};
 
     const auto c = a + b;
 
-    CHECK(c.low() == 2.f);
-    CHECK(c.high() == 15.f);
+    CHECK(c.low() == 2);
+    CHECK(c.high() == 15);
 }
 
 TEST_CASE("subtraction") {
-    const auto a = pbrt::Intervalf{0.f, 5.f};
-    const auto b = pbrt::Intervalf{2.f, 10.f};
+    const auto a = Interval{0, 5};
+    const auto b = Interval{2, 10};
 
     const auto c = a - b;
 
-    CHECK(c.low() == -10.f);
-    CHECK(c.high() == 3.f);
+    CHECK(c.low() == -10);
+    CHECK(c.high() == 3);
 }
 
 TEST_CASE("negation") {
-    const auto a = pbrt::Intervalf{2.f, 10.f};
+    const auto a = Interval{2, 10};
 
     const auto b = -a;
 
-    CHECK(b.low() == -10.f);
-    CHECK(b.high() == -2.f);
+    CHECK(b.low() == -10);
+    CHECK(b.high() == -2);
 }
 
 TEST_CASE("withLow") {
-    const auto interval = pbrt::Intervalf{0.f, 3.f};
+    const auto interval = Interval{0, 3};
 
     SUBCASE("with higher argument than high") {
-        const auto i = interval.withLow(4.f);
+        const auto i = interval.withLow(4);
 
-        CHECK(i.low() == 3.f);
-        CHECK(i.high() == 4.f);
+        CHECK(i.low() == 3);
+        CHECK(i.high() == 4);
     }
 
     SUBCASE("with lower argument than high") {
-        const auto i = interval.withLow(1.f);
+        const auto i = interval.withLow(1);
 
-        CHECK(i.low() == 1.f);
-        CHECK(i.high() == 3.f);
+        CHECK(i.low() == 1);
+        CHECK(i.high() == 3);
     }
 }
 
 TEST_CASE("withHigh") {
-    const auto interval = pbrt::Intervalf{0.f, 3.f};
+    const auto interval = Interval{0, 3};
 
     SUBCASE("with higher argument than high") {
-        const auto i = interval.withHigh(4.f);
+        const auto i = interval.withHigh(4);
 
-        CHECK(i.low() == 0.f);
-        CHECK(i.high() == 4.f);
+        CHECK(i.low() == 0);
+        CHECK(i.high() == 4);
     }
 
     SUBCASE("with lower argument than high") {
-        const auto i = interval.withHigh(1.f);
+        const auto i = interval.withHigh(1);
 
-        CHECK(i.low() == 0.f);
-        CHECK(i.high() == 1.f);
+        CHECK(i.low() == 0);
+        CHECK(i.high() == 1);
     }
 }
