5db7e127364565511e0cf17bcd61c18f5351f672
IDragnev <iddragnev@gmail.com>
2020-11-12 23:08:13 +0200

    Move AtomicFloat to a separate header
    Moved AtomicFloat out of core/Parallel.hpp as the first step of making
    parallel a separate target.

--- include/core/Parallel.hpp
+++ include/core/Parallel.hpp
@@ -8,39 +8,6 @@
 #include <condition_variable>
 
 namespace idragnev::pbrt {
-    class AtomicFloat
-    {
-    private:
-#ifdef RT_FLOAT_AS_DOUBLE
-        using UnderlyingType = std::uint64_t;
-#else
-        using UnderlyingType = std::uint32_t;
-#endif
-
-    public:
-        AtomicFloat() noexcept : AtomicFloat(0.f) {}
-        explicit AtomicFloat(const Float f) noexcept : bits(floatToBits(f)) {}
-
-        operator Float() const noexcept { return bitsToFloat(bits); }
-
-        Float operator=(const Float f) noexcept {
-            bits = floatToBits(f);
-            return f;
-        }
-
-        void add(const Float f) noexcept {
-            UnderlyingType oldBits = bits;
-            UnderlyingType newBits;
-
-            do {
-                newBits = floatToBits(bitsToFloat(oldBits) + f);
-            } while (!bits.compare_exchange_weak(oldBits, newBits));
-        }
-
-    private:
-        std::atomic<UnderlyingType> bits;
-    };
-
     // Simple one-use barrier; ensures that multiple threads all reach a
     // particular point of execution before allowing any of them to proceed
     // past it.
