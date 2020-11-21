03f1980420860e185af21a96fb7988bccd593022
IDragnev <iddragnev@gmail.com>
2020-11-21 16:56:51 +0200

    Add the Material interface

---
+++ include/core/Material.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "core.hpp"
+#include "memory/MemoryArena.hpp"
+
+namespace idragnev::pbrt {
+    enum class TransportMode
+    {
+        radiance,
+        importance,
+    };
+
+    class Material
+    {
+    public:
+        virtual ~Material() = default;
+
+        virtual void
+        computeScatteringFunctions(SurfaceInteraction& interaction,
+                                   memory::MemoryArena& arena,
+                                   const TransportMode mode,
+                                   const bool allowMultipleLobes) const = 0;
+    };
+} // namespace idragnev::pbrt
