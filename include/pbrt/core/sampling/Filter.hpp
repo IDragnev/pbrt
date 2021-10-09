62099df2ec5bc7d2a7f20ffd497741a77444535f
Iliyan <iddragnev@gmail.com>
2021-10-09 16:07:53 +0300

    Add the Filter interface

---
+++ include/pbrt/core/sampling/Filter.hpp
@@ -0,0 +1,18 @@
+#include "pbrt/core/core.hpp"
+#include "pbrt/core/math/Vector2.hpp"
+
+namespace idragnev::pbrt::sampling {
+    class Filter
+    {
+    public:
+        Filter(const Vector2f& radius)
+            : radius(radius)
+            , invRadius(Vector2f(1.f / radius.x, 1.f / radius.y)) {}
+        virtual ~Filter() = default;
+
+        virtual Float eval(const Point2f& p) const = 0;
+
+        const Vector2f radius;
+        const Vector2f invRadius;
+    };
+} // namespace idragnev::pbrt::sampling
